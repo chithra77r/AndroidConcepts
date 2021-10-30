e1df7259b1b87a110feb30cd252b9bdadb011465
chithra <chithracool13@gmail.com>
2021-10-30 14:33:11 +0530

    dynamic fragment added

--- FragmentDemo/app/src/main/java/com/example/fragmentdemo/FragmentDynamic.java
+++ FragmentDemo/app/src/main/java/com/example/fragmentdemo/FragmentDynamic.java
@@ -9,12 +9,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class FragmentStatic extends Fragment {
+public class FragmentDynamic extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-       View view = inflater.inflate(R.layout.fragment_static,container,false);
+       View view = inflater.inflate(R.layout.fragment_dynamic,container,false);
         return view;
     }
 }
--- FragmentDemo/app/src/main/java/com/example/fragmentdemo/MainActivity.java
+++ FragmentDemo/app/src/main/java/com/example/fragmentdemo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.fragmentdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addFrag();
     }
+
+    public void addFrag(){
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction  transaction = manager.beginTransaction();
+        FragmentDynamic dynamic = new FragmentDynamic();
+        transaction.add(R.id.fragcontainer,dynamic);
+        transaction.commit();
+
+    }
+
 }
