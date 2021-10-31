14bf7f9d2b4e67a97ff49278fa00a5aee7132c06
chithra <chithracool13@gmail.com>
2021-10-31 10:57:30 +0530

    Activity and fragment lifecycle tested

--- FragmentDemo/app/src/main/java/com/example/fragmentdemo/FragmentDynamic.java
+++ FragmentDemo/app/src/main/java/com/example/fragmentdemo/FragmentDynamic.java
@@ -1,6 +1,8 @@
 package com.example.fragmentdemo;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,10 +13,77 @@ import androidx.fragment.app.Fragment;
 
 public class FragmentDynamic extends Fragment {
 
+    private static final String COMMON_TAG = "CombinedLifeCycle";
+    private static final String FRAGMENT_NAME = FragmentDynamic.class.getSimpleName();
+
+    private static final String TAG = COMMON_TAG;
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Log.i(TAG, FRAGMENT_NAME + " onAttach");
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, FRAGMENT_NAME + " onCreate");
+    }
+
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-       View view = inflater.inflate(R.layout.fragment_dynamic,container,false);
-        return view;
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        Log.i(TAG, FRAGMENT_NAME + " onCreateView");
+        return inflater.inflate(R.layout.fragment_dynamic, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        Log.i(TAG, FRAGMENT_NAME + " onActivityCreated");
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        Log.i(TAG, FRAGMENT_NAME + " onStart");
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        Log.i(TAG, FRAGMENT_NAME + " onResume");
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        Log.i(TAG, FRAGMENT_NAME + " onPause");
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        Log.i(TAG, FRAGMENT_NAME + " onStop");
+        super.onStop();
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        Log.i(TAG, FRAGMENT_NAME + " onDestroyView");
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i(TAG, FRAGMENT_NAME + " onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDetach() {
+        Log.i(TAG, FRAGMENT_NAME + " onDetach");
+        super.onDetach();
     }
 }
--- FragmentDemo/app/src/main/java/com/example/fragmentdemo/MainActivity.java
+++ FragmentDemo/app/src/main/java/com/example/fragmentdemo/MainActivity.java
@@ -1,27 +1,78 @@
 package com.example.fragmentdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String COMMON_TAG = "CombinedLifeCycle";
+    private static final String ACTIVITY_NAME = MainActivity.class.getSimpleName();
+    private static final String TAG = COMMON_TAG;
+
+
+    private Button buttonAddFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        addFrag();
+
+        buttonAddFragment = (Button) findViewById(R.id.buttonAddFragment);
+
+        buttonAddFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addFragment();
+            }
+        });
+        Log.i(TAG, ACTIVITY_NAME + " onCreate");
     }
 
-    public void addFrag(){
-        FragmentManager manager = getSupportFragmentManager();
-        FragmentTransaction  transaction = manager.beginTransaction();
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, ACTIVITY_NAME + " onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, ACTIVITY_NAME + " onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, ACTIVITY_NAME + " onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, ACTIVITY_NAME + " onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, ACTIVITY_NAME + " onDestroy");
+    }
+
+    private void addFragment() {
         FragmentDynamic dynamic = new FragmentDynamic();
-        transaction.add(R.id.fragcontainer,dynamic);
-        transaction.commit();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragcontainer, dynamic, "demofragment");
+        fragmentTransaction.addToBackStack("fragmentStack1");
+        fragmentTransaction.commit();
 
     }
-
 }
